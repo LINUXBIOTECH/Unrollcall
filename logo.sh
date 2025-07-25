eafc1d8fc5eb43b486da6e6253567e086fbcf7ef
LINUXBIOTECH <arnavlinuxguy@gmail.com>
2025-07-25 11:36:13 -0700

    Add files via upload

---
+++ logo.sh
@@ -0,0 +1,26 @@
+clear
+echo -e "\e[34m##################################################################################################"
+sleep 1
+echo -e "\e[32mF.L.U.SH\e[0m is starting up"
+sleep .5
+echo -e "\e[31mWARNING: FLUSH MAY CAUSE IRREVERSIBLE DAMAGE TO CHROME OS"
+sleep .5
+echo -e "\e[33mNOTE: FLUSH NEEDS AN INTERNET CONNECTION TO FUNCTION"
+sleep .5
+echo -e "\e[36mFFFFFFFFFFFFFFFFFFFFFFLLLLLLLLLLL             UUUUUUUU     UUUUUUUU   SSSSSSSSSSSSSSS HHHHHHHHH     HHHHHHHHH
+F::::::::::::::::::::FL:::::::::L             U::::::U     U::::::U SS:::::::::::::::SH:::::::H     H:::::::H
+F::::::::::::::::::::FL:::::::::L             U::::::U     U::::::US:::::SSSSSS::::::SH:::::::H     H:::::::H
+FF::::::FFFFFFFFF::::FLL:::::::LL             UU:::::U     U:::::UUS:::::S     SSSSSSSHH::::::H     H::::::HH
+  F:::::F       FFFFFF  L:::::L                U:::::U     U:::::U S:::::S              H:::::H     H:::::H  
+  F:::::F               L:::::L                U:::::D     D:::::U S:::::S              H:::::H     H:::::H  
+  F::::::FFFFFFFFFF     L:::::L                U:::::D     D:::::U  S::::SSSS           H::::::HHHHH::::::H  
+  F:::::::::::::::F     L:::::L                U:::::D     D:::::U   SS::::::SSSSS      H:::::::::::::::::H  
+  F:::::::::::::::F     L:::::L                U:::::D     D:::::U     SSS::::::::SS    H:::::::::::::::::H  
+  F::::::FFFFFFFFFF     L:::::L                U:::::D     D:::::U        SSSSSS::::S   H::::::HHHHH::::::H  
+  F:::::F               L:::::L                U:::::D     D:::::U             S:::::S  H:::::H     H:::::H  
+  F:::::F               L:::::L         LLLLLL U::::::U   U::::::U             S:::::S  H:::::H     H:::::H  
+FF:::::::FF           LL:::::::LLLLLLLLL:::::L U:::::::UUU:::::::U SSSSSSS     S:::::SHH::::::H     H::::::HH
+F::::::::FF           L::::::::::::::::::::::L  UU:::::::::::::UU  S::::::SSSSSS:::::SH:::::::H     H:::::::H
+F::::::::FF           L::::::::::::::::::::::L    UU:::::::::UU    S:::::::::::::::SS H:::::::H     H:::::::H
+FFFFFFFFFFF           LLLLLLLLLLLLLLLLLLLLLLLL      UUUUUUUUU       SSSSSSSSSSSSSSS   HHHHHHHHH     HHHHHHHHH"
+
